86b4ffbb22df4e5a5aa5bbdc83f909fe128a6ed9
Houssnou <hosni.moubarak@gmail.com>
2024-11-30 05:36:40 -0500

    added login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "./LoginForm";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="flex items-center justify-center align-middle min-h-screen vertical-center" >
+      <LoginForm />
+    </div>
+  );
 }
