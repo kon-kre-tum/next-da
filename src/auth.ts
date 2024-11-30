a9f64abd150961139ff07fa3ffe9b038f57ea821
Houssnou <hosni.moubarak@gmail.com>
2024-11-30 07:20:24 -0500

    configured db using prisma and postgre

---
+++ src/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import authConfig from "./auth.config"
+import { prisma } from "./app/lib/prisma"
+
+
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
